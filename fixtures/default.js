c04edef989504bfd9bccc37dceeef67eefc0c5a0
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-27 15:59:27 +0200

    refactor

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,7 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                VENUE_ID: '45ac12d6f964a5205d411fe3'
             }
         }
     }
